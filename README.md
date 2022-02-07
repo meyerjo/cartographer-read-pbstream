93499dd500513f799ab559612f870a2101fdfb9b
meyerjo <meyerjo@tf.uni-freiburg.de>
2022-02-07 14:51:09 +0100

    adding requirements.txt adding README.md adding trajectory to main.py adding gitignore

--- README.md
+++ README.md
@@ -9,4 +9,27 @@ Export the proto description from cartographer
 
 ## Print info
 
-``python main.py info --inputfile=<*.pbstream file>`` to print a summary
+``python main.py info --inputfile=<*.pbstream file>`` to print a summary about a file. 
+
+This is an example output for the ``b3-2016-04-05-14-14-00.bag`` used in this
+[demo](https://google-cartographer-ros.readthedocs.io/en/latest/assets_writer.html)
+
+```
+Info about: ./b3-2016-04-05-14-14-00.bag.pbstream
+Serialization Header-Format Version: 2
+Fieldname                            	#Entries
+Field: pose_graph                    	     1 entry
+Field: all_trajectory_builder_options	     1 entry
+Field: submap                        	   136 entries
+Field: node                          	 21679 entries
+Field: trajectory_data               	     1 entry
+Field: imu_data                      	305059 entries
+
+Process finished with exit code 0
+```
+
+## Plot trajectory
+
+``python main.py trajectory --inputfile=<*.pbstream file>`` plots the trajectory as open3d
+
+![Resulting image](imgs/screenshot.png)
--- main.py
+++ main.py
@@ -1,23 +1,55 @@
 import argparse
+import copy
 from collections import defaultdict
 
 from pbstream_reader import PBstream_Reader
 
+import open3d as o3d
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', choices=['info', 'iterate'])
+    parser.add_argument(
+        'action', choices=['info', 'trajectory'], help='action "info" prints some information about the map, "trajectory" plots the trajectory  ')
     parser.add_argument('--inputfile', type=str)
     ARGS = parser.parse_args()
 
     if ARGS.action == 'info':
         PBstream_Reader.info(ARGS.inputfile)
-    elif ARGS.action == 'iterate':
+    elif ARGS.action == 'trajectory':
         loaded = defaultdict(list)
         with PBstream_Reader(ARGS.inputfile) as reader:
             for msg in reader:
-                # print(msg)
                 fields = msg.ListFields()
                 if len(fields) == 0:
                     continue
                 for (field_descriptor, message) in fields:
                     loaded[field_descriptor.name].append(message)
+
+        def get_trans_rot(p):
+            mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+            return (p.translation.x, p.translation.y, p.translation.z), mesh.get_rotation_matrix_from_quaternion(
+                (p.rotation.w, p.rotation.x, p.rotation.y, p.rotation.z))
+
+        # adding Visualizer
+        points, segments = [], []
+        vis = o3d.visualization.Visualizer()
+        vis.create_window()
+        for i, map in enumerate(loaded['pose_graph'][0].trajectory[0].submap):
+            mesh = o3d.geometry.TriangleMesh.create_coordinate_frame(size=5 if i == 0 else 3)
+            trans_xyz, rotate_wxyz = get_trans_rot(map.pose)
+            points.append(trans_xyz)
+            if len(points) > 1:
+                segments.append([i-1, i])
+            mesh.translate(trans_xyz)
+            mesh.rotate(rotate_wxyz)
+
+            vis.add_geometry(mesh)
+
+        colors = [[0, 0, 0]] * len(points)
+
+        line_set = o3d.geometry.LineSet()
+        line_set.points = o3d.utility.Vector3dVector(points)
+        line_set.lines = o3d.utility.Vector2iVector(segments)
+        line_set.colors = o3d.utility.Vector3dVector(colors)
+        vis.add_geometry(line_set)
+        vis.run()
